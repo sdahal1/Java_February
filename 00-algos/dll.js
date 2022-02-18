97b4d5426025a4552688c64fb169ffa129a7154f
Saurabh Dahal <saurabhdahal@Saurabhs-MacBook-Pro2.local>
2022-02-18 08:16:52 -0800

    login reg

--- 00-algos/dll.js
+++ 00-algos/dll.js
@@ -94,14 +94,52 @@ class DLL{
                 this.head.prev=null;
             }
             
+        }
 
+       reverse(){
+            //check if the head and the tail are the same, if so, we can return b/c you cnat reverse if there is only one or no things
+            if(this.head==this.tail){
+                return this;
+            }
 
+            //create a temp variable to point to the node next to the head
+            let temp = this.head.next;
+
+            //create a runner variable and set it to be the head
+            let runner = this.head;
+            
+            //while runner is pointing to a node
+            while(runner!=null){
+                //set the current node (runner)'s next property to be its previous property
+                runner.next = runner.prev;
+
+                //set the runners prev property to be the temp variable
+                runner.prev = temp;
+
+                //if the temp is null, then set runner = temp and temp = the next node from runner
+                if(temp!=null){
+                runner = temp;
+                temp = runner.next;
+                }
+                //otherwise if temp is null, set runner = temp
+                else{
+                    runner=temp;
+                }
+            }
+            //outside the while loop, set temp to be the head
+            temp = this.head;
+
+            //set the head to be the tail
+            this.head=this.tail;
+
+            //set the tail to be temp
+            this.tail = temp;
         }
 }
 
 mylist = new DLL();
 
-// mylist.addToBack(10).addToBack(20).addToBack(30);
-mylist.addToFront(9)
-mylist.removeFromBack();
-mylist.removeFromFront();
+mylist.addToBack(10).addToBack(20).addToBack(30).addToBack(40)
+mylist.display()
+mylist.reverse()
+mylist.display()
